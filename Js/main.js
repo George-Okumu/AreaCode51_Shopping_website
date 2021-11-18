8b87363ced97a19d41ab29907c4f0aabaec74b4d
George Okumu <gokumu12@gmail.com>
2021-11-18 09:57:04 +0300

    plus display

--- Js/main.js
+++ Js/main.js
@@ -1,33 +1,50 @@
 //Add Item
-$('add-to-cart').click(function(e) {
-    e.preventDefault();
+$('.add-to-cart').click(function(event) {
+    event.preventDefault();
     var name = $(this).data('name');
-    var price = $(this).data('price');
-    //add item to cart
-    //will display the added thingy
+    var price = parseInt($(this).data('price'));
+    shoppingLaptop.addItemToCart(name, price, 1);
+    displayTheCart();
 });
-
 //clear items
 $('.clear-cart').click(function() {
-    //call the shopping function and call .clear on it
-    ////display the list
+    shoppingLaptop.clearCart();
+    displayTheCart();
 });
 
 //function for displaying everything to the user
 function displayTheCart() {
     var cartArray = shoppingLaptop.listCart();
-};
+    var output = "";
+    for (var i in cartArray) {
+        output += "<tr>" +
+            "<td>" + cartArray[i].name + "</td>" +
+            "<td>(" + cartArray[i].price + ")</td>" +
+            "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>" +
+            "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>" +
+            "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>" +
+            "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>" +
+            " = " +
+            "<td>" + cartArray[i].total + "</td>" +
+            "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(shoppingLaptop.totalCart());
+    $('.total-count').html(shoppingLaptop.totalCount());
+}
+
 
 //Overall function that host major things
 var shoppingLaptop = (function() {
+
     cart = [];
 
     //constructor
-    function Item(name, price) {
+    function Item(name, price, count) {
         this.name = name;
         this.price = price;
+        this.count = count;
     }
-
     //store the values to session storage
     function saveCart() {
         sessionStorage.setItem('shoppingLaptop', JSON.stringify(cart));
@@ -35,60 +52,82 @@ var shoppingLaptop = (function() {
 
     //Get items / Load added items from session storage
     function loadCart() {
-        cart = JSON.parse(sessionStorage.getItem('shoppingLaptp'))
+        cart = JSON.parse(sessionStorage.getItem('shoppingLaptop'));
     }
+    if (sessionStorage.getItem("shoppingLaptop") != null) {
+        loadCart();
+    }
+
 
     var obj = {};
     //Add item to cart
-    obj.addItemTocart = function(name, price) {
-        var item = new Item(name, price);
-        cart.push(item);
-        saveCart();
-    }
-
-    //Total function
-    obj.totalCart = function() {
-        var totalCart = 0;
-        //plus plus plus
-        return parseFloat(totalCart).toFixed(2);
-    }
-
-    // List of the items held here
-    obj.listCart = function() {
-        var newCartArray = [];
-        for (i in cart) {
-            item = cart[i];
-            newCartArray = {};
-
-            for (j in item) {
-                newCartArray[j] = item[j];
+    obj.addItemToCart = function(name, price, count) {
+            for (var item in cart) {
+                if (cart[item].name === name) {
+                    cart[item].count++;
+                    saveCart();
+                    return;
+                }
             }
-            newCartArray.total = parseFloat(item.price).toFixed(2);
+            var item = new Item(name, price, count);
+            cart.push(item);
+            saveCart();
         }
-        return newCartArray;
-    }
-
-
-    //Remove the entire object that contains that name
-    obj.removeAllItem() = function(name) {
-        for (var item in cart) {
-            if (cart[item].name === name) {
-                cart.splice(item, 1);
+        // Set count from item
+    obj.setCountForItem = function(name, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
                 break;
             }
         }
-        saveCart();
-    }
+    };
+    // Remove single item from an array of single ordered multiple times product
 
-    //clear Entire cart
+
+    // Remove the whole ordered object from cart
+
+
+    // Clear cart
     obj.clearCart = function() {
         cart = [];
         saveCart();
     }
 
+    obj.totalCount = function() {
+        var totalCount = 0;
+        for (var item in cart) {
+            totalCount += cart[item].count;
+        }
+        return totalCount;
+    }
+
+    // Total cart
+    obj.totalCart = function() {
+            var totalCart = 0;
+            for (var item in cart) {
+                totalCart += cart[item].price * cart[item].count;
+            }
+            return parseFloat(totalCart.toFixed(2));
+        }
+        // List of the items held here
+    obj.listCart = function() {
+        var newCartArray = [];
+        for (i in cart) {
+            item = cart[i];
+            newItem = {};
+            for (j in item) {
+                newItem[j] = item[j];
+
+            }
+            newItem.total = parseFloat(item.price * item.count).toFixed(2);
+            newCartArray.push(newItem)
+        }
+        return newCartArray;
+    }
+
+
     return obj;
 })();
 
-// what remain display the content
-//define another function to deletethe added item
-// dfine another function for counting the added item
+displayTheCart();
